9462d774bd8ad09f735c8607c8b2f31df29e0d34
n3taway <n3taway@gmail.com>
2024-02-23 18:56:48 +0800

    测试接口调用

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { Telegraf } from 'telegraf';
-
 import { about } from './commands';
 import { greeting } from './text';
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import { development, production } from './core';
+const superagent = require('superagent');
+
 
 const BOT_TOKEN = process.env.BOT_TOKEN || '';
 const ENVIRONMENT = process.env.NODE_ENV || '';
@@ -17,5 +18,16 @@ bot.on('message', greeting());
 export const startVercel = async (req: VercelRequest, res: VercelResponse) => {
   await production(req, res, bot);
 };
+console.log(123);
+
+async function main() {
+  const res = await superagent.get(process.env.M_URL)
+  .set("Cookie", process.env.M_COOKIE)
+  .set("Host", process.env.M_HOST)
+  
+  console.log('🚧 -> file: index.ts。 res: ', res.text);
+}
+
+main();
 //dev mode
 ENVIRONMENT !== 'production' && development(bot);
